5c3a6a783880da5bd1573646bceba0bfc93b2e49
gunessumre <73123396+gunessumre@users.noreply.github.com>
2024-12-19 17:28:20 +0300

    tamamlandi

---
+++ Data/Context.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FirstLibrary2.Models;
+
+namespace Week_12_Code_First_2.Library.Data
+{
+    internal class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=Week-12-Code-First-2;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<Post>().HasKey(p => p.Id);
+
+            modelBuilder.Entity<User>().HasMany(u => u.Posts).WithOne(p => p.User);
+        }
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstLibrary2.Models
+{
+    internal class Post
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+        public required string UserId { get; set; }
+        public User User { get; set; }
+    }
+}
